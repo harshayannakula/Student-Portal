94a3e87a88812bb15acea379d76e194dde75e479
bhumika1331 <sharmabhumika54@gmail.com>
2025-07-01 11:26:03 +0530

    Application Status Fields added

--- internal/application.go
+++ internal/application.go
@@ -4,11 +4,15 @@ type ApplicationStatus int
 
 const (
 	Applied ApplicationStatus = iota
-
+	ShortListed
+	Cleared
+	Selected
+	Rejected
 )
+
 type Application struct {
 	id int
 	Drive
 	Applicant
-	status ApplicationStatus 
+	status ApplicationStatus
 }
