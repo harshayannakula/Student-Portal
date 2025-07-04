b43b840b2d338cc8b57f600d328fedc26a7518d9
Bhumika Sharma <130481601+bhumika1331@users.noreply.github.com>
2025-07-04 15:45:20 +0530

    Update student_service.go
    Added defer file.Close() to close the function

--- internal/student_service.go
+++ internal/student_service.go
@@ -56,6 +56,7 @@ func SerializeStudents(filename string, students []Student) error {
 	if err != nil {
 		return err
 	}
+	defer file.Close()
 
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(students)
